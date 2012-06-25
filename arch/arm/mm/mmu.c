4e115bf566bf0a9797266144c469f3139a4c350b
Catalin Marinas <catalin.marinas@arm.com>
2012-06-25 14:59:38 +0100

    ARM: 7436/1: Do not map the vectors page as write-through on UP systems
    The vectors page has been traditionally mapped as WT on UP systems but
    this creates a mismatched alias with the directly mapped RAM that is
    using WB attributes. On newer processors like Cortex-A15 this has
    implications on the data/instructions coherency at the point of
    unification (usually L2).
    
    This patch removes such restriction.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Tested-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -493,12 +493,6 @@ static void __init build_mem_type_table(void)
 	vecs_pgprot = kern_pgprot = user_pgprot = cp->pte;
 
 	/*
-	 * Only use write-through for non-SMP systems
-	 */
-	if (!is_smp() && cpu_arch >= CPU_ARCH_ARMv5 && cachepolicy > CPOLICY_WRITETHROUGH)
-		vecs_pgprot = cache_policies[CPOLICY_WRITETHROUGH].pte;
-
-	/*
 	 * Enable CPU-specific coherency if supported.
 	 * (Only available on XSC3 at the moment.)
 	 */
@@ -509,6 +503,7 @@ static void __init build_mem_type_table(void)
 		mem_types[MT_MEMORY_NONCACHED].prot_sect |= PMD_SECT_S;
 		mem_types[MT_MEMORY_NONCACHED].prot_pte |= L_PTE_SHARED;
 	}
+
 	/*
 	 * ARMv6 and above have extended page tables.
 	 */
