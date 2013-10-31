52c7e60ebcfca7db073df2df39e6e94098c72f89
Pawan Kumar <pavaku@codeaurora.org>
2013-10-31 16:19:44 +0530

    fbmem: Check failure of FBIOPUTCMAP ioctl
    On FBIOPUTCMAP ioctl failure deallocate
    fb cmap. Put null check for cmap red, green,
    blue component.
    
    Change-Id: I10468ee30d0e76c256cf3d7a6ffe14db7fd4511b
    Signed-off-by: Pawan Kumar <pavaku@codeaurora.org>

--- drivers/video/fbcmap.c
+++ drivers/video/fbcmap.c
@@ -169,6 +169,9 @@ int fb_copy_cmap(const struct fb_cmap *from, struct fb_cmap *to)
 	if (!to || !from)
 		return -EINVAL;
 
+	if (!to || !from)
+		return -EINVAL;
+
 	if (to->start > from->start)
 		fromoff = to->start - from->start;
 	else
@@ -198,6 +201,9 @@ int fb_cmap_to_user(const struct fb_cmap *from, struct fb_cmap_user *to)
 	int size;
 
 	if (!to || !from || (int)(to->start) < 0)
+		return -EINVAL;
+
+	if (!to || !from)
 		return -EINVAL;
 
 	if (to->start > from->start)
