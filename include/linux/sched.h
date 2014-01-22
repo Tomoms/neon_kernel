44506caf701e960b16ceb2450782fb1738abc4f1
Sai Gurrappadi <sgurrappadi@nvidia.com>
2014-01-21 16:41:37 -0800

    sched: Force sleep on consecutive sched_yields
    If a task sched_yields to itself continuously, force the task to
    sleep in sched_yield. This will lower the CPU load of this task
    thereby lowering the cpu frequency and improving power.
    
    Added a stat variable to track how many times we sleep due these
    consecutive sched_yields. Also added sysctl knobs to control the number
    of consecutive sched_yields before which the sleep kicks in and the
    duration fo the sleep in us.
    
    Bug 1424617
    
    Change-Id: Ie92412b8b900365816e17237fcbd0aac6e9c94ce
    Signed-off-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    Reviewed-on: http://git-master/r/358455
    Reviewed-by: Wen Yi <wyi@nvidia.com>
    Reviewed-by: Peter Zu <pzu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1274,6 +1274,7 @@ struct task_struct {
 	atomic_t usage;
 	unsigned int flags;	/* per process flags, defined below */
 	unsigned int ptrace;
+	unsigned int yield_count;
 
 #ifdef CONFIG_SMP
 	struct llist_node wake_entry;
@@ -2125,6 +2126,8 @@ extern unsigned int sysctl_sched_wake_to_idle;
 extern unsigned int sysctl_sched_ravg_window;
 extern unsigned int sysctl_sched_wakeup_load_threshold;
 extern unsigned int sysctl_sched_window_stats_policy;
+extern unsigned int sysctl_sched_yield_sleep_duration;
+extern int sysctl_sched_yield_sleep_threshold;
 
 #ifdef CONFIG_SCHED_AUTOGROUP
 extern void sched_autogroup_create_attach(struct task_struct *p);
