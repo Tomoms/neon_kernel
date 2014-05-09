69cf26a8b30036552f31d42f1d19aa6a438bf04d
Michael Kerrisk <mtk.manpages@gmail.com>
2014-05-09 16:54:33 +0200

    sched: Simplify return logic in sched_read_attr()
    Gotos are chained pointlessly here, and the 'out' label
    can be dispensed with.
    
    Signed-off-by: Michael Kerrisk <mtk.manpages@gmail.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/536CEC29.9090503@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4779,7 +4779,7 @@ static int sched_read_attr(struct sched_attr __user *uattr,
 
 		for (; addr < end; addr++) {
 			if (*addr)
-				goto err_size;
+				return -EFBIG;
 		}
 
 		attr->size = usize;
@@ -4789,12 +4789,7 @@ static int sched_read_attr(struct sched_attr __user *uattr,
 	if (ret)
 		return -EFAULT;
 
-out:
-	return ret;
-
-err_size:
-	ret = -E2BIG;
-	goto out;
+	return 0;
 }
 
 /**
