67a80e1301b8a5b9e76fcb81f9488dde2ba7da19
Johan Hedberg <johan.hedberg@intel.com>
2014-09-18 11:26:32 +0300

    Bluetooth: Fix setting correct security level when initiating SMP
    commit 5eb596f55cacc2389554a8d7572d90d5e9d4269d upstream.
    
    We can only determine the final security level when both pairing request
    and response have been exchanged. When initiating pairing the starting
    target security level is set to MEDIUM unless explicitly specified to be
    HIGH, so that we can still perform pairing even if the remote doesn't
    have MITM capabilities. However, once we've received the pairing
    response we should re-consult the remote and local IO capabilities and
    upgrade the target security level if necessary.
    
    Without this patch the resulting Long Term Key will occasionally be
    reported to be unauthenticated when it in reality is an authenticated
    one.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    [lizf: Backported to 3.4: adjust context]
    Signed-off-by: Zefan Li <lizefan@huawei.com>

--- net/bluetooth/smp.c
+++ net/bluetooth/smp.c
@@ -329,9 +329,18 @@ static int tk_request(struct l2cap_conn *conn, u8 remote_oob, u8 auth,
 		return 0;
 	}
 
+<<<<<<< HEAD
 	/* MITM is now officially requested, but not required */
 	/* Determine what we need (if anything) from the agent */
 	method = gen_method[local_io][remote_io];
+=======
+	/* Not Just Works/Confirm results in MITM Authentication */
+	if (method != JUST_CFM) {
+		set_bit(SMP_FLAG_MITM_AUTH, &smp->smp_flags);
+		if (hcon->pending_sec_level < BT_SECURITY_HIGH)
+			hcon->pending_sec_level = BT_SECURITY_HIGH;
+	}
+>>>>>>> 2d150da34ec6... Bluetooth: Fix setting correct security level when initiating SMP
 
 	BT_DBG("tk_method: %d", method);
 
