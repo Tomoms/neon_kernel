cf9951049033c97390931d61a0a09d69a9207a3d
Naveen Ramaraj <nramaraj@codeaurora.org>
2014-10-13 18:27:50 -0700

    cpufreq: Always allow update of user policy
    In the cases where the system boots up in a constraint with policy min and
    max lower than cpuinfo min/max, and user tries to set a higher user
    policy min, the value would be overridden during the verifying the
    limits.
    
    Once user initiates the sysfs write the previous user policy is
    maintained in policy min and max thus changing the limits for verification
    of the current policy. Once the verification is completed restore the
    current user policy min/max with the updated values if any. This would take
    care of cases uwhere user policy min/max input is higher/lesser than the
    current min and max.
    
    Bug: 17930235
    Change-Id: I5ad92ba05162cb5c32c3ba3fdae21d2e505493d3
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -513,6 +513,9 @@ static ssize_t store_##file_name					\
 	if (ret)							\
 		return -EINVAL;						\
 									\
+	new_policy.min = new_policy.user_policy.min;			\
+	new_policy.max = new_policy.user_policy.max;			\
+									\
 	ret = sscanf(buf, "%u", &new_policy.object);			\
 	if (ret != 1)							\
 		return -EINVAL;						\
@@ -523,7 +526,9 @@ static ssize_t store_##file_name					\
 	else								\
 		atomic_set(&user_max, new_policy.max);			\
 									\
-	policy->user_policy.object = new_policy.object;			\
+	policy->user_policy.min = new_policy.min;			\
+	policy->user_policy.max = new_policy.max;			\
+									\
 	ret = cpufreq_set_policy(policy, &new_policy);		\
 									\
 	return ret ? ret : count;					\
