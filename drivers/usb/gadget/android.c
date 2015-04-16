10a39943e527b4521b31035c7db137eab856b619
Azhar Shaikh <azhars@codeaurora.org>
2015-04-16 11:52:48 -0700

    USB: android: Fix memory leak in mass_storage_function_init()
    mass_storage_function_init() calls fsg_common_init() which allocates
    memory to fsg buffers only once during bootup. This memory is never
    freed, which results in a memory leak. The reference count is
    incremented in mass_storage_function_init() and in fsg_bind_config().
    The count incremented in bind_config is decremented in fsg_unbind().
    Free this memory and also decrement the reference count in
    mass_storage_function_cleanup() which will be called during gadget unbind.
    
    Signed-off-by: Azhar Shaikh <azhars@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -2034,6 +2034,10 @@ error:
 
 static void mass_storage_function_cleanup(struct android_usb_function *f)
 {
+	struct mass_storage_function_config *config;
+
+	config = f->config;
+	fsg_common_put(config->common);
 	kfree(f->config);
 	f->config = NULL;
 }
