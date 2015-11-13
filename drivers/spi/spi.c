eef74e691b2942f105a87e43fe0ac7d5dcee93c1
Dorimanx <yuri@bynet.co.il>
2015-11-13 16:51:22 +0200

    spi: Ensure memory used for spi_write_then_read() is DMA safe
    Use GFP_DMA in order to ensure that the memory we allocate for transfers
    in spi_write_then_read() can be DMAed. On most platforms this will have
    no effect.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1665,10 +1665,22 @@ int spi_write_then_read(struct spi_device *spi,
 	struct spi_transfer	x[2];
 	u8			*local_buf;
 
-	/* Use preallocated DMA-safe buffer.  We can't avoid copying here,
-	 * (as a pure convenience thing), but we can keep heap costs
-	 * out of the hot path ...
+	/* Use preallocated DMA-safe buffer if we can.  We can't avoid
+	 * copying here, (as a pure convenience thing), but we can
+	 * keep heap costs out of the hot path unless someone else is
+	 * using the pre-allocated buffer or the transfer is too large.
 	 */
+#ifdef CONFIG_SND_SOC_WM5110
+	/* LGE_CHANGE, yeri.lee@lge.com, 2013-10-31, wm5110 Bring up */
+	if ((n_tx + n_rx) > SPI_BUFSIZ || !mutex_trylock(&lock)) {
+		local_buf = kmalloc(max((unsigned int)SPI_BUFSIZ, (unsigned int)(n_tx + n_rx)),
+				    GFP_KERNEL | GFP_DMA);
+		if (!local_buf)
+			return -ENOMEM;
+	} else {
+		local_buf = buf;
+	}
+#else
 	if ((n_tx + n_rx) > SPI_BUFSIZ)
 		return -EINVAL;
 
@@ -1685,9 +1697,9 @@ int spi_write_then_read(struct spi_device *spi,
 
 	/* ... unless someone else is using the pre-allocated buffer */
 	if (!mutex_trylock(&lock)) {
-		local_buf = kmalloc(SPI_BUFSIZ, GFP_KERNEL);
-		if (!local_buf)
-			return -ENOMEM;
+	      local_buf = kmalloc(SPI_BUFSIZ, GFP_KERNEL | GFP_DMA);
+	      if (!local_buf)
+	          return -ENOMEM;
 	} else
 		local_buf = buf;
 
