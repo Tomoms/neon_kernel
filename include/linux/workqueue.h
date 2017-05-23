fe9ff0b124ead37a6637b0197db7bae1bff5782d
Alex Naidis <alex.naidis@linux.com>
2017-05-23 17:50:14 +0200

    workqueue: Implement delayed_work_busy()
    work_busy() was available for regular work only,
    however it is useful for delayed work too.
    
    This implements a variant of work_busy() for
    delayed work.
    
    Change-Id: I7bd6e4f2e3ba9c4517a7eb4635ec8c7dd4c68f38
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- include/linux/workqueue.h
+++ include/linux/workqueue.h
@@ -627,6 +627,15 @@ static inline bool __deprecated flush_delayed_work_sync(struct delayed_work *dwo
 	return flush_delayed_work(dwork);
 }
 
+/**
+ * delayed_work_busy - See work_busy()
+ * @dwork: the delayed work to be tested
+ */
+static inline unsigned int delayed_work_busy(struct delayed_work *dwork)
+{
+	return work_busy(&dwork->work);
+}
+
 #ifndef CONFIG_SMP
 static inline long work_on_cpu(int cpu, long (*fn)(void *), void *arg)
 {
