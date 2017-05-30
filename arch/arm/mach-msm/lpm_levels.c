921e6c2d124dfc8226131ca14d33622bad4ef6d4
Evan Anderson <evananderson@aospa.co>
2017-05-30 02:10:29 -0500

    ARM: mach-msm: Fix numerous section mismatches
    * Qualcomm writes great code /s
    
    Change-Id: Ibfc1cf5196c9ee33e2f06b689d22976eb1109868
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- arch/arm/mach-msm/lpm_levels.c
+++ arch/arm/mach-msm/lpm_levels.c
@@ -1131,7 +1131,7 @@ static struct of_device_id cpu_modes_mtch_tbl[] = {
 	{},
 };
 
-static struct platform_driver cpu_modes_driver = {
+static struct platform_driver cpu_modes_driver __refdata = {
 	.probe = lpm_cpu_probe,
 	.driver = {
 		.name = "cpu-modes",
@@ -1145,7 +1145,7 @@ static struct of_device_id system_modes_mtch_tbl[] = {
 	{},
 };
 
-static struct platform_driver system_modes_driver = {
+static struct platform_driver system_modes_driver __refdata = {
 	.probe = lpm_system_probe,
 	.driver = {
 		.name = "system-modes",
@@ -1159,7 +1159,7 @@ static struct of_device_id lpm_levels_match_table[] = {
 	{},
 };
 
-static struct platform_driver lpm_levels_driver = {
+static struct platform_driver lpm_levels_driver __refdata = {
 	.probe = lpm_probe,
 	.driver = {
 		.name = "lpm-levels",
