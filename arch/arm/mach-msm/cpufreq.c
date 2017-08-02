97f38a662a251a0b2743e9bbecba7c3b537f2925
Tomoms <fondatommaso2@gmail.com>
2017-08-02 11:11:05 +0200

    volt table

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -519,7 +519,7 @@ static int cpufreq_parse_dt(struct device *dev)
 	if (!dts_freq_table)
 		return -ENOMEM;
 
-	*dts_freq_table = *ftbl;
+	*dts_freq_table = *freq_table;
 
 	for (i = 0; i < nf; i++)
 		dts_freq_table[i].frequency = data[i];
