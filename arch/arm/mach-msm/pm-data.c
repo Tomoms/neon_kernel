79409a588808798d5f6917799db8efe34c42dd2e
Tomoms <fondatommaso2@gmail.com>
2017-08-25 10:33:20 +0200

    Revert "msm: enable all idle/suspend modes"
    This reverts commit 1d5fcb134fd40d1793a3e827d99e7d1ed760ba1f.

--- arch/arm/mach-msm/pm-data.c
+++ arch/arm/mach-msm/pm-data.c
@@ -17,21 +17,21 @@ struct msm_pm_platform_data msm_pm_sleep_modes[] = {
 	[MSM_PM_MODE(0, MSM_PM_SLEEP_MODE_POWER_COLLAPSE)] = {
 		.idle_supported = 1,
 		.suspend_supported = 1,
-		.idle_enabled = 1,
-		.suspend_enabled = 1,
+		.idle_enabled = 0,
+		.suspend_enabled = 0,
 	},
 
 	[MSM_PM_MODE(0, MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE)] = {
 		.idle_supported = 1,
 		.suspend_supported = 1,
-		.idle_enabled = 1,
-		.suspend_enabled = 1,
+		.idle_enabled = 0,
+		.suspend_enabled = 0,
 	},
 
 	[MSM_PM_MODE(0, MSM_PM_SLEEP_MODE_RETENTION)] = {
 		.idle_supported = 1,
 		.suspend_supported = 0,
-		.idle_enabled = 1,
+		.idle_enabled = 0,
 		.suspend_enabled = 0,
 	},
 
