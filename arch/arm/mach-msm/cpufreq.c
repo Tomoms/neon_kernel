1bf1ec58d8bc07bb2bef559989290fde96c9bb70
Tomoms <fondatommaso2@gmail.com>
2018-03-20 17:41:36 +0100

    qcom-cpufreq: increase priority

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -335,6 +335,7 @@ static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 
 static struct notifier_block __refdata msm_cpufreq_cpu_notifier = {
 	.notifier_call = msm_cpufreq_cpu_callback,
+	.priority = INT_MAX,
 };
 
 static int msm_cpufreq_suspend(void)
